aaab1dbdaa0c39c596045ae2f8ee2d733c6fcfc5
ali <alikasiri77@gmail.com>
2022-05-02 09:30:55 +0430

    change the templates

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
 ]
